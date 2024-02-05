5b37df55f9404cfab83c3989683f9c6849874c86
MichalShabi <michal.shabi@gmail.com>
2024-02-05 16:07:12 +0200

    add js exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(name) {
+const stringArr = name.split("");
+let newName = "";
+let arrLength = stringArr.length;
+for (let i = 0; i < arrLength; i++) {
+    newName += stringArr.pop();
+}
 
+return newName;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp-32)*(5/9);
+  temp = temp.toFixed(1);
+  return Number(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * (9/5)+32);
+  temp = temp.toFixed(1);
+  return Number(temp);
 };
 
 // Do not edit below this line
